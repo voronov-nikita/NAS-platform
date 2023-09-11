44ad260f7da4447795cf57c34d4bad2433d0036f
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-09-12 00:05:20 +0300

    add-file styles and add TextField in app

--- lib/login.dart
+++ lib/login.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_print, constant_identifier_names
+// ignore_for_file: avoid_print, constant_identifier_names, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 import 'styles.dart';
@@ -6,8 +6,13 @@ import 'styles.dart';
 class MainLogin extends StatelessWidget {
   const MainLogin({super.key});
 
+
   @override
   Widget build(BuildContext context) {
+
+    String userInputLogin = '';
+    String userInputPassword = '';
+
     return Scaffold(
       // основное тело
       body: Center(
@@ -15,12 +20,27 @@ class MainLogin extends StatelessWidget {
         child: Column(
           // ценрирование 
           mainAxisAlignment: MainAxisAlignment.center,
-          // конопки 
           children:[
+            
+            // строка с логином
+            TextField(
+              onChanged: (value){
+                userInputLogin = value;
+              },
+              decoration: LoginStyles.textinputlogin,
+              ),
+            // строка с паролем
+            TextField(
+              onChanged: (value){
+                userInputPassword = value;
+              },
+              decoration: LoginStyles.textinputpassword,
+              ),
+
             // кнопка авторизации
             ElevatedButton(
               onPressed: () {
-                print("войти");
+                print("$userInputLogin - $userInputPassword");
               },
               style: LoginStyles.buttonStyle,
               child: const Text('Войти'),
---
+++ lib/styles.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+
+
+class LoginStyles{
+
+
+  // <-------------- Стиль для кнопок -------------->
+  static ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+    tapTargetSize: MaterialTapTargetSize.padded,
+    foregroundColor: Colors.white, 
+    backgroundColor: Colors.black,
+    textStyle: TextStyle(fontSize: 18),
+    padding: EdgeInsets.all(16.0),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(0.0),
+    ),
+  );
+
+// <--------- Стили для ввода текста --------->
+
+  static InputDecoration textinput = InputDecoration(
+    // стиль при активном фокусе
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(10.0),
+      borderSide: BorderSide(color: Colors.green),
+    ),
+  );
+
+  // текст для логина
+  static InputDecoration textinputlogin = textinput.copyWith(
+    // параметры для текста
+    labelText: 'Login',
+    hintText: 'Login',
+  );
+
+  // текст для пароля
+  static InputDecoration textinputpassword = textinput.copyWith(
+    // параметры для текста
+    labelText: 'Password',
+    hintText: 'Password',
+  );
+}
