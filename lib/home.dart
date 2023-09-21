d71aa6e31951011e3c5a8910c50886d3f3f7ee97
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-09-22 00:08:45 +0300

    add-button

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, avoid_print
 
 import "package:flutter/material.dart";
 
@@ -9,6 +9,38 @@ class HomePage extends StatelessWidget{
     return Scaffold(
       body: Center(
         child: Text("hello"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => CreateNewArticle()),
+          );
+        },
+        backgroundColor: Colors.black,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0), // Радиус закругления углов
+        ),
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
+  }
+}
+
+
+
+class CreateNewArticle extends StatelessWidget{
+  const CreateNewArticle({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Новая Запись'),
+      ),
+      body: Center(
+        
       ),
     );
   }
